03ece99b90ec802cd6573d87cf1982cf8f363592
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-10-31 14:24:23 -0400

    exercise 5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum('annual_revenue')
+puts Store.average('annual_revenue')
+puts Store.where(Store.arel_table[:annual_revenue].gt(1000000)).count
